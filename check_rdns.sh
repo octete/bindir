d5c58e49df992adfe361b222da1b90fe158341bb
Jesus Roncero <jesus@we7.com>
2013-03-18 16:07:04 +0000

    Different if it's an IP or a hostname.
    So that we can pass a hostname and it gives both the ip it resolves to
    and the rDNS that IP resolves to, or the IP, and we only get the rDNS.

--- check_rdns.sh
+++ check_rdns.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Check that a given host resolves to an IP and it resovles back to the same thing.
 #
@@ -10,16 +10,34 @@ if [[ -z "$HOST" ]] ; then
     exit 1
 fi
 
-export ip=$(getent hosts $HOST | awk '{print $1}') 
-if [[ "$?" != "0" ]]  ; then
-    echo "Could not find forward DNS for host $HOST" >&2
-    exit 1
-fi
+export HOST
 
-export rdns=$(getent hosts $ip | awk '{print $2}') 
-if [[ "$?" != "0" ]]  ; then
-    echo "Could not find reverse DNS for host $HOST with IP $ip" >&2
-    exit 1
+if  echo $HOST | grep -q -P "\d+\.\d+\.\d+\.\d"  ; then
+    #echo "IP"
+    rdns=$(getent hosts $HOST | awk '{print $2}')
+    if [[ "$?" != "0" ]]  ; then
+        echo "Could not find reverse DNS for host $HOST with IP $ip" >&2
+        exit 1
+    fi
+    export rdns
+    echo "Host $HOST resolves back to $rdns"
+else
+    #echo "Hostname. Forward DNS"
+    ip=$(getent hosts $HOST | awk '{print $1}')
+    if [[ "$?" != "0" ]]  ; then
+        echo "Could not find forward DNS for host $HOST" >&2
+        exit 1
+    fi
+    export ip
+    rdns=$(getent hosts $ip | awk '{print $2}')
+    if [[ "$?" != "0" ]]  ; then
+        echo "Could not find reverse DNS for host $HOST with IP $ip" >&2
+        exit 1
+    fi
+    export rdns
+    echo "Host $HOST resolves to IP: $ip which resolves back to $rdns"
 fi
+    # it's not an IP Address
 
-echo "Host $HOST resolves to IP: $ip which resolves back to $rdns"
+
+
